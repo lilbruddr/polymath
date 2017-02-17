13a4455a434fe701a3ce07043445ccf20c48527f
Josiah Hartley <josiah.hartley@gmail.com>
2017-02-17 17:29:33 -0500

    Added calendar

---
+++ polymath/calendar_gen.py
@@ -0,0 +1,44 @@
+
+import datetime
+from calendar import monthrange
+
+def calendar():
+    today = datetime.date.today()
+    this_month = today.month
+    this_year = today.year
+    this_day = today.day
+
+    if this_month == 1 or this_month == 2:
+        this_month += 12
+        this_year -= 1
+
+    century = this_year // 100
+    last_two_digits = this_year % 100
+
+    sum = 1 + int(2.6*this_month - 5.39) + century // 4 + last_two_digits // 4 + last_two_digits - 2*century
+    
+    first_day_of_month = sum % 7
+    
+    this_month = today.month
+    this_year = today.year
+
+    num_days = max(monthrange(this_year,this_month))
+    num_weeks = ((num_days + first_day_of_month) // 7) + 1
+
+    month = [[]]
+    day_i = 0
+    for empty in range(first_day_of_month):
+        month[0].append(0)
+    for day_of_first_week in range(first_day_of_month,7):
+        day_i = day_of_first_week - first_day_of_month + 1
+        month[0].append(day_i)
+    for week in range(1,num_weeks):
+        month.append([])
+        for day in range(7):
+            if day_i < num_days:
+                day_i += 1
+                month[week].append(day_i)
+            else:
+                month[week].append(0)
+
+    return month
--- polymath/views.py
+++ polymath/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 #from django.template import loader
+import polymath.calendar_gen as cal
 
 def index(request):
     return HttpResponse("Hello, world.  You're at the polls index.")
 
+class Student():
+    def __init__(self):
+        self.first_name = "Josiah"
+        self.last_name = "Hartley"    
+
 def dashboard(request):
-    context = {}
+    user = Student()
+    month = cal.calendar()
+    context = {
+        'user': user,
+        'month': month
+    }
     return render(request, 'polymath/dashboard.html', context)
