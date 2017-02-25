fe16bf1f0e68ad63128d3ec7d267810027149b67
Josiah Hartley <josiah.hartley@gmail.com>
2017-02-25 15:28:41 -0500

    Added overall grade graph

--- polymath/extra_functions.py
+++ polymath/extra_functions.py
@@ -1,6 +1,43 @@
 
 import datetime
 from calendar import monthrange
+from math import pi, sin, cos
+
+def grade_svg_gen(overall_grade, dimensions=150):
+    """Generate an SVG grade donut chart, given a Student's grades.
+
+    Returns a string with the svg.
+    """
+    center_dim = dimensions // 2
+    radius = center_dim - 5
+    font_size_in_px = (radius - 20) // 2
+
+    if overall_grade > 50: # sets which direction the sweep should occur
+        dir_flag = 1
+    else:
+        dir_flag = 0
+
+    ending_angle = (1 - overall_grade/100)*2*pi
+    if overall_grade >= 100:
+        ending_angle = 2*pi+0.01
+    if overall_grade < 0:
+        ending_angle = 0
+
+    ending_position_x = center_dim + radius*cos(ending_angle)
+    ending_position_y = center_dim - radius*sin(ending_angle)
+
+    svg = """<span style="font-family: 'Open Sans'; font-size:""" + str(font_size_in_px) + """px"><svg width='""" + str(dimensions) + """' height='""" + str(dimensions) + """'>
+                <circle cx='""" + str(center_dim) + """' cy='""" + str(center_dim) + """' r='""" + str(radius) + """' stroke="#fff" stroke-width="4" fill="#BDBDBD" />
+                <path d="M """ + str(center_dim + radius) + " " + str(center_dim) + """
+                        A """ + str(radius) + " " + str(radius) + """ 0 """ + str(dir_flag) + " 1 " + str(ending_position_x) + " " + str(ending_position_y) + """
+                        L """ + str(center_dim) + " " + str(center_dim) + """
+                        L """ + str(center_dim + radius) + " " + str(center_dim) + """ " stroke="#fff" fill="#1B5E20" stroke-width="4"/>
+                <circle cx='""" + str(center_dim) + """' cy='""" + str(center_dim) + """' r='""" + str(radius - 20) + """' stroke="#fff" stroke-width="4" fill="white" />
+                <text x='""" + str(center_dim) + """' y='""" + str(center_dim) + """' font-family="Open Sans;" text-anchor="middle" dominant-baseline="middle">""" + str('{:.1f}'.format(overall_grade)) + """%</text>
+            </svg></span>"""
+
+    return svg
+
 
 def calendar(given_day=datetime.date.today()):
     """Generate a calendar for the month containing the given date.
--- polymath/views.py
+++ polymath/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 #from django.template import loader
 from polymath.extra_functions import calendar
+from polymath.extra_functions import grade_svg_gen
 import datetime
 
 def index(request):
@@ -17,10 +18,12 @@ def dashboard(request):
     highlighted_day = datetime.date.today()
     user = Student()
     month = calendar(highlighted_day)
+    grades = grade_svg_gen(46.333, 150)
     context = {
         'user': user,
         'month': month,
         'header_month': highlighted_day.strftime('%B %Y'),
         'bold_day': highlighted_day.day,
+        'grade_plot': grades,
     }
     return render(request, 'polymath/dashboard.html', context)
